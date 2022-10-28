81094d6e3e26b181e6e6cc993c4b408625af82ff
Yoshi Debat <yoshidebat@gmail.com>
2022-10-28 16:54:20 -0300

    agregar el guardado del token de ejemplo en el localStorage

--- src/Components/views/Auth/LoginForm.js
+++ src/Components/views/Auth/LoginForm.js
@@ -16,6 +16,7 @@ const LoginForm = () => {
     onSubmit: (values) => {
       console.log(values);
       formik.resetForm();
+      localStorage.setItem("token", "tokenValueExample");
     },
   });
 
