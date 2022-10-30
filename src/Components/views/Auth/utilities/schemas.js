d5a1bddfb5e1c5a38698c0c9a50064d985f14be8
Yoshi Debat <yoshidebat@gmail.com>
2022-10-30 16:23:55 -0300

    agregar esquema de validacion del formulario de registro

--- src/Components/views/Auth/utilities/schemas.js
+++ src/Components/views/Auth/utilities/schemas.js
@@ -15,3 +15,22 @@ export const LoginSchema = Yup.object().shape({
       "La contraseña debe tener al menos 1 letra, 1 número y 1 símbolo"
     ),
 });
+
+export const RegisterSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("El email es obligatorio")
+    .email("El email ingresado no es válido"),
+  password: Yup.string()
+    .required("La contraseña es obligatoria")
+    .min(6, "La contraseña debe tener al menos 6 caracteres")
+    .matches(
+      REGEX_PASSWORD,
+      "La contraseña debe tener al menos 1 letra, 1 número y 1 símbolo"
+    ),
+  confirmPassword: Yup.string()
+    .required("La confirmación de contraseña es obligatoria")
+    .oneOf(
+      [Yup.ref("password"), null],
+      "Las contraseñas ingresadas no coinciden"
+    ),
+});
