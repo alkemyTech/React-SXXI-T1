50c236bacb923ed4ee6a272c954d114469b306b9
Luis Gerez Martinez <luis.gerez@gmail.com>
2022-11-04 09:08:54 -0300

    Feature/ong1 191 (#24)
    * Feature/ong1 190 (#21)
    
    * agregar ruta para formulario de login
    
    * adaptar formulario de login base a react-bootstrap
    
    * borrar import de hoja de estilos
    
    * agregar validacion al formulario de login
    
    * reestructurar carpetas y codigo del formulario de login
    
    * agregar el guardado del token de ejemplo en el localStorage
    
    * arreglar error de tipografia en la propiedad background
    
    * renombrar component Input a InputAuth
    
    * agregar ruta para el formulario de registro
    
    * agregar esquema de validacion del formulario de registro
    
    * aplicar esquema al componente RegisterForm
    
    * Feature/ong1 38 (#9)
    
    * estilos de newsLetter terminados
    
    * Feature/ong1 36 (#8)
    
    * comenzando componente contactus
    
    * modificando componente contactus
    
    * estilizando componente contactus
    
    * finalice el componente contact-us
    
    * funcionalidad al boton ir al inicio
    
    * eliminacion de titulo y boton contribuir
    
    * campo telefono ahora es requerido
    
    * correccion de botones
    
    * primera entrega newsLetter - terminada
    
    Co-authored-by: Cristian <albornozcristian94@gmail.com>
    
    * arreglar problemas luego del merge
    
    * arreglar problemas luego del merge
    
    * Feature/ong1 50 (#14)
    
    * terminada tarea ONG1-48 - creación del componente título
    
    * terminado estilo y logica de componente detalle de actividad
    
    * terminado lo que pide la card ong1-50
    
    * corregido spinner y watning efecto en  useDetail
    
    * Feature/ong1 48 (#11)
    
    * terminada tarea ONG1-48 - creación del componente título
    
    * agregando imagen al fondo del texto
    
    * agregando algunos componentes globales
    
    Co-authored-by: Yoshi Debat <yoshidebat@gmail.com>
    Co-authored-by: Cristian <albornozcristian94@gmail.com>
    Co-authored-by: Paulo César Cuneo <paulocuneo@gmail.com>
    Co-authored-by: Yoshi Debat <47387933+LYoshi02@users.noreply.github.com>
    
    * pausa para pushear servicio axios
    
    Co-authored-by: Yoshi Debat <yoshidebat@gmail.com>
    Co-authored-by: Cristian <albornozcristian94@gmail.com>
    Co-authored-by: Paulo César Cuneo <paulocuneo@gmail.com>
    Co-authored-by: Yoshi Debat <47387933+LYoshi02@users.noreply.github.com>

--- src/Components/views/Private/Slides/styled-components/SlidesForm.styled.js
+++ src/Components/views/Private/Slides/styled-components/SlidesForm.styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import { inputTextCssStyle } from "styled-components/App.styled";
 
 const WrapButtonsForm = styled.div`
+  ${inputTextCssStyle}
   display: flex;
   justify-content: space-evenly;
 `;
