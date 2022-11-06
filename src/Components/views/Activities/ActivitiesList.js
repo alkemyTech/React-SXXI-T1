bd57ed08ce1246138570b9ddf749444626f4f31f
Yoshi Debat <yoshiwebdev@gmail.com>
2022-11-06 10:22:02 -0300

    listar actividades

--- src/Components/views/Activities/ActivitiesList.js
+++ src/Components/views/Activities/ActivitiesList.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import '../CardListStyles.css';
+import React from "react";
+import { Card } from "react-bootstrap";
+import "../CardListStyles.css";
+import { Link } from "react-router-dom";
+
+import { CustomTitle } from "Components/GlobalComponents/CustomTitle/CustomTitle";
 
 const ActivitiesList = () => {
-    const activitiesMock = [
-        {id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba'}
-    ];
+  const activitiesMock = [
+    { id: 1, name: "Titulo de prueba", description: "Descripcion de prueba" },
+    { id: 2, name: "Titulo de prueba", description: "Descripcion de prueba" },
+    { id: 3, name: "Titulo de prueba", description: "Descripcion de prueba" },
+  ];
 
-    return (
-        <div>
-            <h1>Listado Actividades</h1>
-            <ul className="list-container">
-                {activitiesMock.length > 0 ?
-                    activitiesMock.map((activity) => {
-                        return(
-                            <li className="card-info" key={activity.id}>
-                                <h3>{activity.name}</h3>
-                                <p>{activity.description}</p>
-                            </li>
-                        )
-                    })
-                :
-                    <p>No hay actividades</p>
-                }
-            </ul>
-        </div>
-    );
-}
- 
+  return (
+    <div>
+      <CustomTitle
+        wrapTitleClass="d-block h-auto"
+        title="Listado Actividades"
+      />
+      <ul className="d-flex justify-content-around gap-2">
+        {activitiesMock.map((activity) => (
+          <Card key={activity.id}>
+            <Card.Body>
+              <Card.Title>{activity.name}</Card.Title>
+              <Card.Text>{activity.description}</Card.Text>
+              <Card.Link as={Link} to={`/actividades/${activity.id}`}>
+                Ver Actividad
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 export default ActivitiesList;
