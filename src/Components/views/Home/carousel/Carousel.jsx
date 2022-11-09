1187a466ce4861990bb4a8bb4b3142cb919931fd
cris-al <albornozcristian94@gmail.com>
2022-11-09 10:16:51 -0300

    correcciones terminadas

--- src/Components/views/Home/carousel/Carousel.jsx
+++ src/Components/views/Home/carousel/Carousel.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 import { SlideTitle, SlideDescription, SlideImage } from './CarouselStyled/Carousel.Styled';
 import { api } from "Services/axiosService";
+import Swal from "sweetalert2";
 
 export default function CarouselComponent(){
     const [slides, setSlides] = useState([]);
@@ -17,6 +18,14 @@ export default function CarouselComponent(){
             });
             setSlides(slidesData);
         })
+        .catch(()=>{
+            Swal.fire({
+                title: 'Hubo un error',
+                icon: 'error',
+                confirmButtonColor: '#0038FF',
+                confirmButtonText: 'Aceptar',
+            })
+        });
     }, []);
     
     return(
