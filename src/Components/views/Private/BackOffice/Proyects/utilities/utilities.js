d691acc1884fcecfe3c4f7a48e5abde6718e8432
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-11 12:27:03 -0300

    actualizo utilities para que se envie y llegue bien la imagen

--- src/Components/views/Private/BackOffice/Proyects/utilities/utilities.js
+++ src/Components/views/Private/BackOffice/Proyects/utilities/utilities.js
@@ -1,25 +1,47 @@
 import * as Yup from 'yup';
 import Swal from 'sweetalert2';
 import { validationMessages } from 'utilities/validationMessage.util';
+import { FILE_SIZE } from 'Components/GlobalComponents/FormImageField/utilities/ImageFieldSchemas.util';
 
 const FORMAT = ['image/png', 'image/jpg', 'image/jpeg'];
 
-export const validationSchema = Yup.object().shape({
-    title : Yup.string().required(validationMessages.title.required),
-    description: Yup.string().required(validationMessages.description.required),
-    image: Yup.mixed().required(validationMessages.image.required)
-              .test( "fileFormat", "Solo formato .jpg, .jpeg y .png", value => value && FORMAT.includes(value.type)),
-    due_date: Yup.string().required(validationMessages.due_date.required)
-  });
+export const activityValidationSchema = (
+    id
+  ) => {
+    const validationSchema = Yup.object().shape({
+        title : Yup.string().required(validationMessages.title.required),
+        description: Yup.string().required(validationMessages.description.required),
+        image: Yup.mixed()
+            .test(
+                "format", 
+                validationMessages.image.format, (value) => {
+                return id
+                    ? value
+                    : value && FORMAT.includes(value.type);
+                }
+            )
+            .test(
+            "fileSize", 
+            validationMessages.image.fieldSize,
+            (value) =>
+                id ? value : value && value.size <= FILE_SIZE
+            )
+            .required(validationMessages.image.required),
+        due_date: Yup.string().required(validationMessages.due_date.required)
+    });
 
+    return validationSchema;
+}
 
 export const convertToBase64 = (image, setImage) => {
     const reader = new FileReader();
-    reader.readAsDataURL(image);
     
     reader.onloadend = function () {
         setImage(reader.result.toString());
     };
+
+    reader.readAsDataURL(image);
+    
     reader.onerror = function (error) {
         console.log('Error: ', error);
     };
