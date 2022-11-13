13d93ad75860889e24e7adb53fb117f31173f0fa
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-13 16:19:41 -0300

    elimino la parte de usuaiors que estaba duplicada en views

--- src/Components/views/Private/BackOffice/Users/utilities/utilities.js
+++ src/Components/views/Private/BackOffice/Users/utilities/utilities.js
@@ -1,17 +1,34 @@
 import * as Yup from 'yup';
 import { validationMessages } from 'utilities/validationMessage.util';
+import { FILE_SIZE } from 'Components/GlobalComponents/FormImageField/utilities/ImageFieldSchemas.util';
 
 const FORMAT = ['image/png', 'image/jpg', 'image/jpeg'];
+const emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
-export const validationSchema = Yup.object().shape({
-  name : Yup.string()
-    .min(4, validationMessages.name.fieldLength)
-    .required(validationMessages.name.required),
-  email : Yup.string().required(validationMessages.email.required),
-  role_id : Yup.string().required(validationMessages.role_id.required),
-  profile_image: Yup.mixed()
-    .required( validationMessages.image.required)
-    .test( "fileFormat", "Solo formato .png, .jpg y .jpeg",
-      value => value && FORMAT.includes(value.type)
-    )
-});
+export const usersValidationSchema = (
+  id
+) => {
+  const validationSchema = Yup.object().shape({
+    name : Yup.string()
+            .min(4, validationMessages.name.fieldLength)
+            .required(validationMessages.name.required),
+    email: Yup.string().matches(emailRegExp, validationMessages.email.format)
+            .required(validationMessages.email.required),
+    role_id: Yup.string().required(validationMessages.role_id.required),
+    image: Yup.mixed()
+            .test("format", validationMessages.image.format, (value) => {
+              return id
+                ? value
+                : value && FORMAT.includes(value.type);
+              })
+            .test(
+              "fileSize", 
+              validationMessages.image.fieldSize,
+              (value) =>
+                id ? value : value && value.size <= FILE_SIZE
+            )
+            .required(validationMessages.image.required)
+  });
+
+  return validationSchema;
+}
