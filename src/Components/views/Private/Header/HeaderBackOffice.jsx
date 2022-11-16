92fec2d5bd675c196fd63622b1a606016756c7c4
cris-al <albornozcristian94@gmail.com>
2022-11-16 11:35:36 -0300

    borrando comentarios

--- src/Components/views/Private/Header/HeaderBackOffice.jsx
+++ src/Components/views/Private/Header/HeaderBackOffice.jsx
@@ -14,23 +14,5 @@ export default function HeaderBackOffice(){
             </Button>
             <h3>Somos Más</h3>
         </MainContainer>
-        // <Navbar bg='primary' variant='dark'>
-        //     {/* <Container> */}
-        //     {/* <Navbar.Brand href="#home">
-        //         <img
-        //           alt="icono menu"
-        //           src={MenuIcon}
-        //           width="30"
-        //           height="30"
-        //           className="d-inline-block align-top"
-        //         />{' '}
-        //         React Bootstrap
-        //     </Navbar.Brand> */}
-        //         <Button onClick={handleClick}>
-        //             <Icon src={MenuIcon} alt="icono menu"/>
-        //         </Button>
-        //         <h3 className='me-auto'>Somos Más</h3>
-        //     {/* </Container> */}
-        // </Navbar>
     )
 }
