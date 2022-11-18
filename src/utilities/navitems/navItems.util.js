b62e4165cafeecd364da85e6b2c02d81844dcf15
cris-al <albornozcristian94@gmail.com>
2022-11-18 17:14:47 -0300

    corrigiendo rutas en navItems.utils.js

--- src/utilities/navitems/navItems.util.js
+++ src/utilities/navitems/navItems.util.js
@@ -22,12 +22,12 @@ export { footerNavItems, publicHeaderNavItems };
 
 export const privateLinks = [
     { text: 'Dashboard', icon: homeIcon, to: privateRoutes.BACKOFFICE},
-    { text: 'Actividades', icon: activityIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.ACTIVITIESLIST}`},
-    { text: 'Categorias', icon: categoryIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.CATEGORIESLIST}`},
-    { text: 'Miembros', icon: memberIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.MEMBERSLIST}`},
-    { text: 'Novedades', icon: newsIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.NEWSLIST}`},
-    { text: 'Organización', icon: organizationIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.ORGANIAZTIONDATA}`},
-    { text: 'Slides', icon: slideIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.SLIDESLIST}`},
-    { text: 'Testimonios', icon: testimonialIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.TESTIMONIALSLIST}`},
-    { text: 'Usuarios', icon: userIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.USERSLIST}`}
+    { text: 'Actividades', icon: activityIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.ACTIVITIES}`},
+    { text: 'Categorias', icon: categoryIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.CATEGORIES}`},
+    { text: 'Miembros', icon: memberIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.MEMBERS}`},
+    { text: 'Novedades', icon: newsIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.NEWS}`},
+    { text: 'Organización', icon: organizationIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.ORGANIZATIONDATA}`},
+    { text: 'Slides', icon: slideIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.SLIDES}`},
+    { text: 'Testimonios', icon: testimonialIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.TESTIMONIALS}`},
+    { text: 'Usuarios', icon: userIcon, to: `${privateRoutes.BACKOFFICE}/${privateRoutes.USERS}`}
 ];
