1231922731e555b422763c099fff03bdfed038a4
cris-al <albornozcristian94@gmail.com>
2022-11-23 10:02:08 -0300

    haciendo pull de dev y reemplazando las instancias de axios publica y privada segun corresponda

--- src/Services/ContactService.js
+++ src/Services/ContactService.js
@@ -1,12 +1,13 @@
 import axios from "axios";
-import { api } from "./axiosService";
+import { publicApi } from "./publicApiService";
+import { privateApi } from "./privateApiService";
 import { requestMessagesSchema } from 'utilities/requestMessagesSchema.util'
 
 const url = 'contacts';
 
 export const getContacts = async () => {
     try {
-        const { data } = await api(url, {
+        const { data } = await publicApi(url, {
             method: 'get',
         });
         if (!data || !data.success)
@@ -28,7 +29,7 @@ export const getContacts = async () => {
 
 export const getContact = async (id) => {
     try {
-        const { data } = await api(`${url}/${id}`,{
+        const { data } = await publicApi(`${url}/${id}`,{
             method: 'get'
         });
         if (!data || !data.success)
@@ -50,9 +51,9 @@ export const getContact = async (id) => {
 
 export const postContact = async (body) => {
     try {
-        const { data } = await api(url, {
+        const { data } = await privateApi(url, {
             method: 'post',
-            body: body,
+            data: body,
         });
         if (!data || !data.success)
           throw new Error(
@@ -73,9 +74,9 @@ export const postContact = async (body) => {
 
 export const putContact = async (id, body) => {
     try {
-        const { data } = await api(`${url}/${id}`, {
+        const { data } = await privateApi(`${url}/${id}`, {
             method: 'put',
-            body: body,
+            data: body,
         });
         if (!data || !data.success)
           throw new Error(
@@ -96,7 +97,7 @@ export const putContact = async (id, body) => {
 
 export const deleteContact = async (id) => {
     try {
-        const { data } = await api(`${url}/${id}`, {
+        const { data } = await privateApi(`${url}/${id}`, {
             method: 'delete',
         });
         if (!data || !data.success)
