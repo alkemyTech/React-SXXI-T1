4e8494d9f23ff7eb606812954525e805280d473b
cris-al <albornozcristian94@gmail.com>
2022-11-22 23:16:07 -0300

    agregando servicios

--- src/Services/ContactService.js
+++ src/Services/ContactService.js
@@ -1,3 +1,117 @@
-
-
 import axios from "axios";
+import { api } from "./axiosService";
+import { requestMessagesSchema } from 'utilities/requestMessagesSchema.util'
+
+const url = 'contacts';
+
+export const getContacts = async () => {
+    try {
+        const { data } = await api(url, {
+            method: 'get',
+        });
+        if (!data || !data.success)
+          throw new Error(
+            data ? data.message : requestMessagesSchema.problemExistTryLater
+          );
+
+        const dataAdapter = data.data;
+
+        return { success: data.success, data: dataAdapter };
+    } catch (error) {
+        console.error("error interceptor: ", error.message);
+        if (axios.isCancel(error)) {
+          return { message: "solicitud axios cancelada" };
+        }
+        return { success: false, message: error.message };
+    }
+}
+
+export const getContact = async (id) => {
+    try {
+        const { data } = await api(`${url}/${id}`,{
+            method: 'get'
+        });
+        if (!data || !data.success)
+          throw new Error(
+            data ? data.message : requestMessagesSchema.problemExistTryLater
+          );
+
+        const dataAdapter = data.data;
+
+        return { success: data.success, data: dataAdapter };
+    } catch (error) {
+        console.error("error interceptor: ", error.message);
+        if (axios.isCancel(error)) {
+          return { message: "solicitud axios cancelada" };
+        }
+        return { success: false, message: error.message };
+    }
+}
+
+export const postContact = async (body) => {
+    try {
+        const { data } = await api(url, {
+            method: 'post',
+            body: body,
+        });
+        if (!data || !data.success)
+          throw new Error(
+            data ? data.message : requestMessagesSchema.problemExistTryLater
+          );
+
+        const dataAdapter = data.data;
+
+        return { success: data.success, data: dataAdapter };
+    } catch (error) {
+        console.error("error interceptor: ", error.message);
+        if (axios.isCancel(error)) {
+          return { message: "solicitud axios cancelada" };
+        }
+        return { success: false, message: error.message };
+    }
+}
+
+export const putContact = async (id, body) => {
+    try {
+        const { data } = await api(`${url}/${id}`, {
+            method: 'put',
+            body: body,
+        });
+        if (!data || !data.success)
+          throw new Error(
+            data ? data.message : requestMessagesSchema.problemExistTryLater
+          );
+
+        const dataAdapter = data.data;
+
+        return { success: data.success, data: dataAdapter };
+    } catch (error) {
+        console.error("error interceptor: ", error.message);
+        if (axios.isCancel(error)) {
+          return { message: "solicitud axios cancelada" };
+        }
+        return { success: false, message: error.message };
+    }
+}
+
+export const deleteContact = async (id) => {
+    try {
+        const { data } = await api(`${url}/${id}`, {
+            method: 'delete',
+        });
+        if (!data || !data.success)
+          throw new Error(
+            data ? data.message : requestMessagesSchema.problemExistTryLater
+          );
+
+        const dataAdapter = data.data;
+
+        return { success: data.success, data: dataAdapter };
+    } catch (error) {
+        console.error("error interceptor: ", error.message);
+        if (axios.isCancel(error)) {
+          return { message: "solicitud axios cancelada" };
+        }
+        return { success: false, message: error.message };
+    }
+}
