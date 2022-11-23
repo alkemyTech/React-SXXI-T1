167be8da9c1d65323ad34142137d2a48e979642d
cris-al <albornozcristian94@gmail.com>
2022-11-23 10:06:14 -0300

    haciendo pull de dev y reemplazando las instancias de axios publica y privada segun corresponda

--- src/Services/SlidesService.js
+++ src/Services/SlidesService.js
@@ -1,12 +1,13 @@
 import axios from "axios";
-import { api } from "./axiosService";
+import { publicApi } from "./publicApiService";
+import { privateApi } from "./privateApiService";
 import { requestMessagesSchema } from 'utilities/requestMessagesSchema.util'
 
 const url = 'slides';
 
 export const getSlides = async () => {
     try {
-        const { data } = await api(url, {
+        const { data } = await publicApi(url, {
             method: 'get',
         });
         if (!data || !data.success)
@@ -28,7 +29,7 @@ export const getSlides = async () => {
 
 export const getSlide = async (id) => {
     try {
-        const { data } = await api(`${url}/${id}`,{
+        const { data } = await publicApi(`${url}/${id}`,{
             method: 'get'
         });
         if (!data || !data.success)
@@ -50,9 +51,9 @@ export const getSlide = async (id) => {
 
 export const postSlide = async (body) => {
     try {
-        const { data } = await api(url, {
+        const { data } = await privateApi(url, {
             method: 'post',
-            body: body,
+            data: body,
         });
         if (!data || !data.success)
           throw new Error(
@@ -73,9 +74,9 @@ export const postSlide = async (body) => {
 
 export const putSlide = async (id, body) => {
     try {
-        const { data } = await api(`${url}/${id}`, {
+        const { data } = await privateApi(`${url}/${id}`, {
             method: 'put',
-            body: body,
+            data: body,
         });
         if (!data || !data.success)
           throw new Error(
@@ -96,7 +97,7 @@ export const putSlide = async (id, body) => {
 
 export const deleteSlide = async (id) => {
     try {
-        const { data } = await api(`${url}/${id}`, {
+        const { data } = await privateApi(`${url}/${id}`, {
             method: 'delete',
         });
         if (!data || !data.success)
