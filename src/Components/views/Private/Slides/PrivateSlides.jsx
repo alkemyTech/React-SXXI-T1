dcaa12e8ee7dc6e635238640231712c04b6164be
Yoshi Debat <yoshiwebdev@gmail.com>
2022-11-25 19:36:52 -0300

    implementar de busqueda de slides en backoffice

--- src/Components/views/Private/Slides/PrivateSlides.jsx
+++ src/Components/views/Private/Slides/PrivateSlides.jsx
@@ -6,13 +6,41 @@ import { CustomTable } from "Components/GlobalComponents/CustomTable/CustomTable
 import { usePrivateSlides } from "./hooks/usePrivateSlides";
 import { myTableData, tableHead } from "./utilities/slidesSchema.util";
 import { addIcon } from "assets/images";
+import SearchSlides from "./components/SearchSlides/SearchSlides";
+import { SpinnerLoad } from "Components/GlobalComponents/SpinnerLoad/SpinnerLoad";
 
 const PrivateSlides = () => {
-  const { loadSlides, slides, handleEdit, handleDelete } = usePrivateSlides();
+  const { loadSlides, slides, handleEdit, handleDelete, fetchSlides } =
+    usePrivateSlides();
+
+  const searchSlidesHandler = async (searchText) => {
+    const fetchParams = {};
+
+    if (searchText.length >= 3) {
+      fetchParams["search"] = searchText;
+    }
+
+    await fetchSlides(fetchParams);
+  };
+
+  let slidesContent;
+  if (loadSlides) {
+    slidesContent = <SpinnerLoad />;
+  } else {
+    slidesContent = (
+      <CustomTable
+        tHead={tableHead}
+        tBody={slides}
+        myTableData={myTableData}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
+    );
+  }
 
   return (
-    <>
-      <div className=" mt-2 d-flex col col-12">
+    <div>
+      <div className="mt-2 d-flex col col-12">
         <CustomTitle title="Slides" />
       </div>
       <div className="mt-5 d-flex flex-wrap justify-content-center justify-content-sm-between">
@@ -30,15 +58,11 @@ const PrivateSlides = () => {
           icon={addIcon}
         />
       </div>
-      <CustomTable
-        tHead={tableHead}
-        tBody={slides}
-        loading={loadSlides}
-        myTableData={myTableData}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
-      />
-    </>
+      <div className="my-3">
+        <SearchSlides onSearchSlides={searchSlidesHandler} />
+      </div>
+      <div>{slidesContent}</div>
+    </div>
   );
 };
 
