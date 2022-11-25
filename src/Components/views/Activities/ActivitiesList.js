16c0def2de2d0a3646f014ab2502bb3905d51abb
Yoshi Debat <yoshiwebdev@gmail.com>
2022-11-25 19:02:01 -0300

    implementar busqueda de actividades

--- src/Components/views/Activities/ActivitiesList.js
+++ src/Components/views/Activities/ActivitiesList.js
@@ -3,16 +3,38 @@ import React from "react";
 import { CustomTitle } from "Components/GlobalComponents/CustomTitle/CustomTitle";
 import { CustomCard } from "Components/GlobalComponents/CustomCard/CustomCard";
 import { routes } from "models/routes";
+import { useActivities } from "./hooks/useActivities";
+import SearchActivities from "./components/SearchActivities";
+import { SpinnerLoad } from "Components/GlobalComponents/SpinnerLoad/SpinnerLoad";
 
 const ActivitiesList = () => {
-  const activitiesMock = [
-    { id: 1, name: "Titulo de prueba", content: "Descripcion de prueba" },
-    { id: 2, name: "Titulo de prueba", content: "Descripcion de prueba" },
-    { id: 3, name: "Titulo de prueba", content: "Descripcion de prueba" },
-    { id: 4, name: "Titulo de prueba", content: "Descripcion de prueba" },
-    { id: 5, name: "Titulo de prueba", content: "Descripcion de prueba" },
-    { id: 6, name: "Titulo de prueba", content: "Descripcion de prueba" },
-  ];
+  const { loadingActivities, activitiesData, fetchActivities } =
+    useActivities();
+
+  const searchActivitiesHandler = async (searchText) => {
+    const fetchParams = {};
+
+    if (searchText.length >= 3) {
+      fetchParams["search"] = searchText;
+    }
+
+    await fetchActivities(fetchParams);
+  };
+
+  let activitiesContent;
+  if (loadingActivities) {
+    activitiesContent = <SpinnerLoad />;
+  } else {
+    activitiesContent = activitiesData.map((activity) => (
+      <div key={activity.id} className="col col-12 col-sm-6 col-lg-4 mb-3">
+        <CustomCard
+          cardInfo={activity}
+          to={`${routes.ACTIVITIES}/${activity.id}`}
+          grid="oneColumn"
+        />
+      </div>
+    ));
+  }
 
   return (
     <div>
@@ -20,16 +42,11 @@ const ActivitiesList = () => {
         wrapTitleClass="d-block h-auto"
         title="Listado Actividades"
       />
+      <div className="my-3">
+        <SearchActivities onSearchActivities={searchActivitiesHandler} />
+      </div>
       <div className="d-flex flex-row flex-wrap justify-content-center">
-        {activitiesMock.map((activity) => (
-          <div className="col col-12 col-sm-6 col-lg-4 mb-3">
-            <CustomCard
-              cardInfo={activity}
-              to={`${routes.ACTIVITIES}/${activity.id}`}
-              grid="oneColumn"
-            />
-          </div>
-        ))}
+        {activitiesContent}
       </div>
     </div>
   );
---
+++ src/Components/views/Activities/components/SearchActivities.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import { SearchInput } from "Components/GlobalComponents/SearchInput/SearchInput";
+
+const SearchActivities = ({ onSearchActivities }) => {
+  const [searchText, setSearchText] = useState("");
+
+  const changeSearchTextHandler = (searchText) => {
+    setSearchText(searchText);
+  };
+
+  const searchActivitiesHandler = () => {
+    onSearchActivities(searchText);
+  };
+
+  return (
+    <SearchInput
+      searchText={searchText}
+      placeholder="Buscar actividad..."
+      onChangeText={changeSearchTextHandler}
+      onSearch={searchActivitiesHandler}
+    />
+  );
+};
+
+export default SearchActivities;
