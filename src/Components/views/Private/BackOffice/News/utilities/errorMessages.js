0987f8cef046bff432da46b5f60c094315569494
Yoshi Debat <yoshiwebdev@gmail.com>
2022-11-30 10:52:39 -0300

    agregar mensajes de error para el backoffice de novedades

---
+++ src/Components/views/Private/BackOffice/News/utilities/errorMessages.js
@@ -0,0 +1,6 @@
+export const errorMessages = {
+  getNews: "Se produjo un error al obtener las novedades",
+  getCategories: "Se produjo un error al obtener las categorias",
+  createNews: "Se produjo un error al crear la novedad",
+  editNews: "Se produjo un error al editar la novedad",
+}
