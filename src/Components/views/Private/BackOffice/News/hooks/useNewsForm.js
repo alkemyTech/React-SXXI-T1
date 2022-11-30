45a039213d7d985ed705ecf33b0d15e564794c60
Yoshi Debat <yoshiwebdev@gmail.com>
2022-11-30 10:53:14 -0300

    agregar alerts de error a peticiones de novedades en backoffice

--- src/Components/views/Private/BackOffice/News/hooks/useNewsForm.js
+++ src/Components/views/Private/BackOffice/News/hooks/useNewsForm.js
@@ -1,150 +1,144 @@
-import { useFormik } from "formik";
-import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom/dist';
-import { useEffect, useState } from "react";
-import { newsValidationSchema } from "../utilities/utilities";
-import { api } from 'Services/axiosService';
-import { convertUrlToBase64 } from "utilities/convertURLtoBase64.util";
-import Alert from "../../components/Alert";
+import { useFormik } from "formik"
+import { useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom/dist"
+import { useEffect, useState } from "react"
+import { newsValidationSchema } from "../utilities/utilities"
+import { api } from "Services/axiosService"
+import { convertUrlToBase64 } from "utilities/convertURLtoBase64.util"
+import Alert from "../../components/Alert"
+import { feedbackUser } from "utilities/alerts/feedbackUser.util"
+import { errorMessages } from "../utilities/errorMessages"
 
 export const useNewsForm = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [ imageBase64, setImageBase64] = useState("");
-  const [ categories, setCategories ] = useState([]);
-  const [ loading, setLoading ] = useState(false);
-  const [ news, setNews ] = useState({
-      name: '',
-      category_id: '',
-      content: '',
-      image: ''
-  });
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [imageBase64, setImageBase64] = useState("")
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [news, setNews] = useState({
+    name: "",
+    category_id: "",
+    content: "",
+    image: "",
+  })
   const initialValues = {
-      name: '',
-      category_id: '',
-      content: '',
-      image: ''
-  };
+    name: "",
+    category_id: "",
+    content: "",
+    image: "",
+  }
 
-  useEffect(()=>{
-      if(id) {
-        api.get(`/news/${id}`)
-        .then(res => {
-          const { data } = res.data;
+  useEffect(() => {
+    if (id) {
+      api
+        .get(`/news/${id}`)
+        .then((res) => {
+          const { data } = res.data
           setNews({
             name: data.name,
             category_id: data.category_id,
             content: data.content,
-            image: data.image
-          });
+            image: data.image,
+          })
         })
         .catch(() => {
-          Alert({ icon: 'error', title: 'Ha ocurrido un error'});
-        });    
-      }
-    },[id]);
+          feedbackUser("center", "error", `${errorMessages.getNews}`)
+        })
+    }
+  }, [id])
 
   useEffect(() => {
-    api.get(`/categories`)
-    .then(res => {
-      const { data } = res.data;
-      const categories = data.map(element => {
-        return {
-          id: element.id,
-          name: element.name
-        }
+    api
+      .get(`/categories`)
+      .then((res) => {
+        const { data } = res.data
+        const categories = data.map((element) => {
+          return {
+            id: element.id,
+            name: element.name,
+          }
+        })
+        setCategories(categories)
       })
-      setCategories( categories);
-    })
-  }, []);
+      .catch(() => {
+        feedbackUser("center", "error", `${errorMessages.getCategories}`)
+      })
+  }, [])
 
-  const backURL = '/backoffice';
+  const backURL = "/backoffice"
 
   const onSubmit = async () => {
-    const { name, content, category_id } = values;
+    const { name, content, category_id } = values
     const body = { name, content, category_id, image: imageBase64 }
 
-    if(id) {
-      const bodyEdit = { 
-        ...news, 
-        ...body, 
-        image: imageBase64 || await convertUrlToBase64(news.image)
+    if (id) {
+      const bodyEdit = {
+        ...news,
+        ...body,
+        image: imageBase64 || (await convertUrlToBase64(news.image)),
       }
-      const alertWarning = await Alert({ icon:'warning', 
-            title:'¿Estas segura/o de enviarlo?', 
-            cancelText: 'Cancelar' })
-      
+      const alertWarning = await Alert({ icon: "warning", title: "¿Estas segura/o de enviarlo?", cancelText: "Cancelar" })
+
       if (alertWarning.isConfirmed) {
-        setLoading(true);
+        setLoading(true)
         const apiResponse = await api.put(`/news/${id}`, bodyEdit)
-        if(apiResponse.data.success) {
-          setLoading(false);
-          await Alert({ icon: 'success', title: 'Operación éxitosa'})
+        if (apiResponse.data.success) {
+          setLoading(false)
+          await Alert({ icon: "success", title: "Operación éxitosa" })
           navigate(backURL)
         } else {
-          await Alert({ icon: 'error', title: 'Ha ocurrido un error'});
+          feedbackUser("center", "error", `${errorMessages.editNews}`)
         }
       }
     } else {
-      const alertWarning = await Alert({ icon:'warning', 
-                title:'¿Estas segura/o de enviarlo?', 
-                cancelText: 'Cancelar' })
+      const alertWarning = await Alert({ icon: "warning", title: "¿Estas segura/o de enviarlo?", cancelText: "Cancelar" })
 
-      if(alertWarning.isConfirmed) {
-        setLoading(true);
+      if (alertWarning.isConfirmed) {
+        setLoading(true)
         const apiResponse = await api.post(`/news`, body)
-        if(apiResponse.data.success) {
-          setLoading(false);
-          await Alert({ icon: 'success', title: 'Operación éxitosa'})
+        if (apiResponse.data.success) {
+          setLoading(false)
+          await Alert({ icon: "success", title: "Operación éxitosa" })
           navigate(backURL)
         } else {
-          await Alert({ icon: 'error', title: 'Ha ocurrido un error'});
+          feedbackUser("center", "error", `${errorMessages.createNews}`)
         }
       }
     }
   }
 
-  const validationSchema = newsValidationSchema(id);
+  const validationSchema = newsValidationSchema(id)
 
-  const formik = useFormik({initialValues, onSubmit, validationSchema});
+  const formik = useFormik({ initialValues, onSubmit, validationSchema })
 
-  const {
-    values, 
-    errors, 
-    handleBlur, 
-    handleSubmit, 
-    handleChange, 
-    touched,
-    setFieldValue 
-  } = formik;
+  const { values, errors, handleBlur, handleSubmit, handleChange, touched, setFieldValue } = formik
 
   useEffect(() => {
     if (Object.keys(news).length > 0) {
-      setFieldValue("name", news.name);
-      setFieldValue("content", news.content);
-      setFieldValue("image", news.image);
-      setFieldValue("category_id", news.category_id);
+      setFieldValue("name", news.name)
+      setFieldValue("content", news.content)
+      setFieldValue("image", news.image)
+      setFieldValue("category_id", news.category_id)
     }
-  }, [news, setFieldValue]);
+  }, [news, setFieldValue])
 
   const cancel = () => {
-    navigate(backURL);
+    navigate(backURL)
   }
 
   return {
-    values, 
-    errors, 
-    handleBlur, 
-    handleSubmit, 
-    handleChange, 
-    touched, 
-    news, 
-    loading, 
-    formik, 
+    values,
+    errors,
+    handleBlur,
+    handleSubmit,
+    handleChange,
+    touched,
+    news,
+    loading,
+    formik,
     cancel,
     setImageBase64,
     setFieldValue,
-    categories
-  } 
-
+    categories,
+  }
 }
