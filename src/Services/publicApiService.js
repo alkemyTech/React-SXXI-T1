cd06df12123cb4e4c822197703d73da6af3691df
Cristian <albornozcristian94@gmail.com>
2022-11-30 09:07:03 -0300

    Feature/ong1 202 (#96)
    * coneccion del servicio al endpoint "organization" con el componente OrganizationData.jsx terminado
    
    * terminando te conectar los componentes CategoriesList, OrganizationData, CategoriesForm y ContactForm con los servicios correspondientes
    
    * corrigiendo errores en CategoriesForm
    
    * terminando coneccion de los componentes CategoriesForm y Contact con servicios respectivos
    
    * Finalizando feature 202

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { requestMessagesSchema } from "utilities/requestMessagesSchema.util"
 
-export const publicApi = axios.create({
+const publicApi = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   timeout: 5000,
 })
@@ -25,5 +25,28 @@ const get = async (restURL) => {
   }
 }
 
-const publicService = { get }
-export default publicService
+const post = async (restURL, body) => {
+  try {
+    const { data } = await publicApi(restURL, {
+        method: 'post',
+        data: body,
+    });
+    if (!data || !data.success)
+      throw new Error(
+        data ? data.message : requestMessagesSchema.problemExistTryLater
+      );
+
+    const dataAdapter = data.data;
+
+    return { success: data.success, data: dataAdapter };
+  } catch (error) {
+      console.error("error interceptor: ", error.message);
+      if (axios.isCancel(error)) {
+        return { message: "solicitud axios cancelada" };
+      }
+      return { success: false, message: error.message };
+  }
+};
+
+const publicService = { get, post };
+export default publicService;
