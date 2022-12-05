02b0ba23b13121dcef004155f87e31649c01714b
cris-al <albornozcristian94@gmail.com>
2022-12-05 15:23:46 -0300

    estados de manejo de size de dispositivo ahora es solo un estado en custom hook useComment y useDetailNews

--- src/Components/views/News/Comment/hook/useComment.js
+++ src/Components/views/News/Comment/hook/useComment.js
@@ -1,67 +1,60 @@
-import { useState } from "react"
-import publicService from "Services/publicApiService"
-import { URLs } from "Services/ServicesURLS"
-import { feedbackUser } from "utilities/alerts/feedbackUser.util"
+import { useState } from "react";
+import publicService from "Services/publicApiService";
+import { URLs } from "Services/ServicesURLS";
+import { feedbackUser } from "utilities/alerts/feedbackUser.util";
 
 export const useComment = () => {
-  const [comments, setComments] = useState([])
-  const [minSize, setMinSize] = useState(false)
-  const [maxSize, setMaxSize] = useState(false)
-  const [medSize, setMedSize] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [pages, setPages] = useState(10)
-  const [req, setReq] = useState(true)
+  const [comments, setComments] = useState([]);
+  const [commentSize, setCommentSize] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [pages, setPages] = useState(10);
+  const [req, setReq] = useState(true);
 
   const textDivider = (text) => {
-    let text1
-    let text2
-    const points = "..."
-    if (text) {
-      if (text.length > 55 && minSize) {
-        text1 = text.slice(0, 42)
-        text2 = text.slice(37)
-        return { text1, text2, points }
-      }
-      if (text.length > 150 && medSize) {
-        text1 = text.slice(0, 145)
-        text2 = text.slice(145)
-        return { text1, text2, points }
-      }
-      if (text.length > 250 && maxSize) {
-        text1 = text.slice(0, 245)
-        text2 = text.slice(245)
-        return { text1, text2, points }
-      }
+    let text1;
+    let text2;
+    const points = "...";
+    if (text.length > 55 && commentSize === "min") {
+      text1 = text.slice(0, 42);
+      text2 = text.slice(37);
+      return { text1, text2, points };
     }
-    return text
-  }
+    if (text.length > 150 && commentSize === "med") {
+      text1 = text.slice(0, 145);
+      text2 = text.slice(145);
+      return { text1, text2, points };
+    }
+    if (text.length > 250 && commentSize === "max") {
+      text1 = text.slice(0, 245);
+      text2 = text.slice(245);
+      return { text1, text2, points };
+    }
+    return text;
+  };
 
   const handleResize = () => {
-    if (window.innerWidth < 490) setMinSize(true)
-    else setMinSize(false)
-    if (window.innerWidth >= 490 && window.innerWidth < 750) setMedSize(true)
-    else setMedSize(false)
-    if (window.innerWidth >= 750) setMaxSize(true)
-    else setMaxSize(false)
-  }
+    if (window.innerWidth < 490) setCommentSize("min");
+    if (window.innerWidth >= 490 && window.innerWidth < 750) setCommentSize("med");
+    if (window.innerWidth >= 750) setCommentSize("max");
+  };
 
   const handleScroll = () => {
     if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
-      setLoading(true)
-      setPages((prev) => prev + 10)
+      setLoading(true);
+      setPages((prev) => prev + 10);
     }
-  }
+  };
 
   const getComments = () => {
-    setLoading(true)
+    setLoading(true);
     if (req) {
       publicService
         .get(`${URLs.comment}?limit=${pages}`)
         .then((res) => {
           if (res.success) {
             if (res.data.length === comments.length) {
-              setReq(false)
-              setLoading(false)
+              setReq(false);
+              setLoading(false);
             } else {
               const info = res.data.map((el) => {
                 return {
@@ -70,16 +63,16 @@ export const useComment = () => {
                   image: el.image,
                   text: el.text,
                   visible: el.visible,
-                }
-              })
-              setComments(info)
-              setLoading(false)
+                };
+              });
+              setComments(info);
+              setLoading(false);
             }
-          } else feedbackUser("center", "error", "Ha ocurrido un error")
+          } else feedbackUser("center", "error", "Ha ocurrido un error");
         })
-        .catch(() => feedbackUser("center", "error", "Ha ocurrido un error"))
-    } else setLoading(false)
-  }
+        .catch(() => feedbackUser("center", "error", "Ha ocurrido un error"));
+    } else setLoading(false);
+  };
 
   return {
     comments,
@@ -93,5 +86,5 @@ export const useComment = () => {
     setReq,
     req,
     getComments,
-  }
-}
+  };
+};
