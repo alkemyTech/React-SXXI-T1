dc62a7dfb017299cea036e6889ebf586693c79c6
Yoshi Debat <yoshiwebdev@gmail.com>
2022-12-05 21:00:55 -0300

    arreglar formulario de organizacion

---
+++ src/Components/views/Private/BackOffice/Organization/OrganizationForm.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+import Input from "../components/Input";
+import { ButtonConfirm } from "./OrganizationForm.Styled";
+import DescriptionEditor from "../components/DescriptionEditor";
+import { CustomTitle } from "Components/GlobalComponents/CustomTitle/CustomTitle";
+import { BackTo } from "Components/GlobalComponents/BackTo/BackTo";
+import { privateRoutes } from "models/routes";
+import { FormImageField } from "Components/GlobalComponents/FormImageField/FormImageField";
+import { useOrganizationForm } from "./hooks/useOrganizationForm";
+
+const OrganizationForm = () => {
+  const { errors, handleSubmit, handleChange, touched, organization, loading, formik, values, setImageBase64, setFieldValue } = useOrganizationForm();
+
+  const shortDescriptionChangeHandler = (data) => {
+    formik.setFieldValue("shortDescription", data);
+  };
+
+  return (
+    <section className="container my-5">
+      <div className="my-5">
+        <CustomTitle title={"Edita Organización"} justify="center" wrapTextClass="text-center" wrapTitleClass="d-block h-auto" />
+      </div>
+      <div className="my-5">
+        <BackTo wrapLink="my-4" to={"/" + privateRoutes.BACKOFFICE + privateRoutes.ORGANIZATIONDATA} />
+      </div>
+      <Form className="my-5 col-sm-10 col-lg-6 mx-auto d-flex flex-column gap-4" onSubmit={handleSubmit}>
+        <Input
+          value={values.name}
+          label="Nombre de la organización:"
+          type="text"
+          name="name"
+          placeholder="Nombre"
+          isTouched={touched.name}
+          error={errors.name}
+          onChange={handleChange}
+        />
+        <FormImageField
+          errors={errors.logo}
+          touched={touched.logo}
+          name="logo"
+          setFieldValue={formik.setFieldValue}
+          setImageToSend={setImageBase64}
+          imageIsEdit={organization.logo}
+        />
+        <DescriptionEditor
+          label="Descripción corta de la organización:"
+          initialData={values.shortDescription}
+          isTouched={touched.shortDescription}
+          error={errors.shortDescription}
+          onChange={shortDescriptionChangeHandler}
+        />
+        <Input
+          value={values.longDescription}
+          label="Descripción larga de la organización:"
+          type="text"
+          name="longDescription"
+          placeholder="Descripción larga"
+          isTouched={touched.longDescription}
+          error={errors.longDescription}
+          onChange={handleChange}
+        />
+        <Input
+          value={values.facebookUrl}
+          label="Facebook de la organización:"
+          type="text"
+          name="facebookUrl"
+          placeholder="Url Facebook"
+          isTouched={touched.facebookUrl}
+          error={errors.facebookUrl}
+          onChange={handleChange}
+        />
+        <Input
+          value={values.linkedinUrl}
+          label="LinkedIn de la organización:"
+          type="text"
+          name="linkedinUrl"
+          placeholder="Url LinkedIn"
+          isTouched={touched.linkedinUrl}
+          error={errors.linkedinUrl}
+          onChange={handleChange}
+        />
+        <Input
+          value={values.instagramUrl}
+          label="Instragram de la organización:"
+          type="text"
+          name="instagramUrl"
+          placeholder="Url Instagram"
+          isTouched={touched.instagramUrl}
+          error={errors.instagramUrl}
+          onChange={handleChange}
+        />
+        <Input
+          value={values.twitterUrl}
+          label="Twitter de la organización:"
+          type="text"
+          name="twitterUrl"
+          placeholder="Url Twitter"
+          isTouched={touched.twitterUrl}
+          error={errors.twitterUrl}
+          onChange={handleChange}
+        />
+        <div className="my-5 d-flex justify-content-center">
+          <ButtonConfirm className="col-7 col-lg-8 py-2 px-3 mx-auto" disabled={loading} background="success" color="success" type="submit">
+            Confirmar
+          </ButtonConfirm>
+        </div>
+      </Form>
+    </section>
+  );
+};
+
+export default OrganizationForm;
---
+++ src/Components/views/Private/BackOffice/Organization/hooks/useOrganizationForm.js
@@ -0,0 +1,132 @@
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom/dist";
+import { useEffect, useState } from "react";
+
+import apiServices from "Services/privateApiService";
+import { URLs } from "Services/ServicesURLS";
+import Alert from "../../components/Alert";
+import { feedbackUser } from "utilities/alerts/feedbackUser.util";
+import { errorMessages } from "../utilities/errorMessages";
+import { EditOrganizationSchema } from "../../utilities/schemas";
+import { convertToBase64 } from "../utilities/utilities";
+
+export const useOrganizationForm = () => {
+  const navigate = useNavigate();
+  const [imageBase64, setImageBase64] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [organization, setOrganization] = useState({
+    name: "",
+    logo: "",
+    shortDescription: "",
+    longDescription: "",
+    facebookUrl: "",
+    linkedinUrl: "",
+    instagramUrl: "",
+    twitterUrl: "",
+  });
+
+  useEffect(() => {
+    apiServices
+      .get(URLs.organization)
+      .then((res) => {
+        const { data } = res;
+        console.log(data);
+        setOrganization({
+          name: data.name,
+          logo: data.logo,
+          shortDescription: data.short_description,
+          longDescription: data.long_description,
+          facebookUrl: data.facebook_url,
+          linkedinUrl: data.linkedin_url,
+          instagramUrl: data.instagram_url,
+          twitterUrl: data.twitter_url,
+        });
+      })
+      .catch((e) => {
+        feedbackUser("center", "error", `${errorMessages.getOrganization}`);
+      });
+  }, []);
+
+  const backURL = "/backoffice";
+
+  const onSubmit = async () => {
+    const { name, shortDescription, longDescription, facebookUrl, instagramUrl, linkedinUrl, twitterUrl } = values;
+    const body = { name, logo: imageBase64, shortDescription, longDescription, facebookUrl, instagramUrl, linkedinUrl, twitterUrl };
+
+    const alertWarning = await Alert({ icon: "warning", title: "¿Segura/o?", cancelText: "Cancelar" });
+    if (alertWarning.isConfirmed) {
+      setLoading(true);
+      const apiResponse = await apiServices.post(`/${URLs.organization}`, body);
+
+      if (apiResponse.success) {
+        setLoading(false);
+        await Alert({ icon: "success", title: "Operación éxitosa" });
+        navigate(backURL);
+      } else {
+        await feedbackUser("center", "error", `${errorMessages.editOrganization}`);
+      }
+
+      setLoading(false);
+    }
+  };
+
+  function handleImage(e) {
+    const image = e.target.files[0];
+    if (image) {
+      formik.setFieldValue("logo", image);
+      convertToBase64(image, setImageBase64);
+    } else {
+      formik.setFieldValue("logo", "");
+    }
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      logo: "",
+      shortDescription: "",
+      longDescription: "",
+      facebookUrl: "",
+      linkedinUrl: "",
+      instagramUrl: "",
+      twitterUrl: "",
+    },
+    validationSchema: EditOrganizationSchema,
+    onSubmit,
+  });
+
+  const { values, errors, handleBlur, handleSubmit, handleChange, touched, setFieldValue } = formik;
+
+  useEffect(() => {
+    if (Object.keys(organization).length > 0) {
+      setFieldValue("name", organization.name);
+      setFieldValue("logo", organization.logo);
+      setFieldValue("shortDescription", organization.shortDescription);
+      setFieldValue("longDescription", organization.longDescription);
+      setFieldValue("facebookUrl", organization.facebookUrl);
+      setFieldValue("linkedinUrl", organization.linkedinUrl);
+      setFieldValue("instagramUrl", organization.instagramUrl);
+      setFieldValue("twitterUrl", organization.twitterUrl);
+    }
+  }, [organization, setFieldValue]);
+
+  const cancel = () => {
+    navigate(backURL);
+  };
+
+  return {
+    values,
+    errors,
+    handleBlur,
+    handleSubmit,
+    handleChange,
+    touched,
+    organization,
+    loading,
+    formik,
+    handleImage,
+    cancel,
+    setImageBase64,
+    setFieldValue,
+  };
+};
