0a4afdeb49fa4b9156a29907b0cb2abd16b568de
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-12-06 14:03:36 -0300

    paso los handler al hook para que el componente que renderiza el listado de novedades quede mas claro

--- src/Components/views/Private/BackOffice/News/List/NewsList.jsx
+++ src/Components/views/Private/BackOffice/News/List/NewsList.jsx
@@ -1,67 +1,17 @@
-import { useNavigate } from "react-router-dom";
-
 import { addIcon } from "assets/images";
 import { BackTo } from "Components/GlobalComponents/BackTo/BackTo";
 import { CustomTable } from "Components/GlobalComponents/CustomTable/CustomTable";
 import { CustomTitle } from "Components/GlobalComponents/CustomTitle/CustomTitle";
-import { feedbackUser } from "utilities/alerts/feedbackUser.util";
-import { handleUserConfirm as AlertWarning } from "utilities/alerts/userConfirm.util";
 import { privateRoutes } from "models/routes";
 import SearchNews from "./components/SearchNews";
 import { useNews } from "./hooks/useNews";
 import { SpinnerLoad } from "Components/GlobalComponents/Loading/SpinnerLoad/SpinnerLoad";
-import privateService from "Services/privateApiService";
-import { URLs } from "Services/ServicesURLS";
-import { successMessages } from "../utilities/successMessages";
-import { errorMessages } from "../utilities/errorMessages";
-import { useState } from "react";
 
 const NewsList = () => {
   const tHead = ["#", "Nombre", "Imagen", "Fecha de Creación", "Acciones"];
   const myTableData = { name: "name", image: "image", created_at: "created_at" };
-  const { loadingNews, newsData, fetchNews } = useNews();
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-
-  const editHandler = (id) => {
-    navigate(`/${privateRoutes.BACKOFFICE}${privateRoutes.NEWSEDITFORM}${id}`);
-  };
-
-  const deleteHandler = async (id) => {
-    const find = newsData.find((element) => id === element.id);
-
-    if (find) {
-      const response = await AlertWarning("¿Estas segura/o que deseas eliminar la novedad?");
-
-      if (response) {
-        setLoading(true);
-        const activityDeleted = await privateService.deleted(URLs.news, id);
-
-        if (activityDeleted) {
-          await feedbackUser("center", "success", `${successMessages.deleteNews}`);
-          fetchNews();
-        } else {
-          await feedbackUser("center", "error", `${errorMessages.deleteNews}`);
-        }
-      }
-      setLoading(false);
-    }
-  };
-
-  const searchNewsHandler = async (searchText, selectedCategory) => {
-    const fetchParams = {};
-
-    if (selectedCategory.length > 0) {
-      fetchParams["category"] = selectedCategory;
-    }
-
-    if (searchText.length >= 3) {
-      fetchParams["search"] = searchText;
-    }
-
-    await fetchNews(fetchParams);
-  };
-
+  const { loadingNews, editHandler, deleteHandler, searchNewsHandler,newsData } = useNews();
+   
   let newsContent;
   if (loadingNews) {
     newsContent = <SpinnerLoad />;
--- src/Components/views/Private/BackOffice/News/List/hooks/useNews.js
+++ src/Components/views/Private/BackOffice/News/List/hooks/useNews.js
@@ -1,15 +1,60 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { feedbackUser } from "utilities/alerts/feedbackUser.util";
 import { filterNews } from "../../utilities/utilities";
+import { handleUserConfirm as AlertWarning } from "utilities/alerts/userConfirm.util";
 import { URLs } from "Services/ServicesURLS";
 import privateService from "Services/privateApiService";
 import { encodeQueryParams } from "utilities/queryParams";
 import { errorMessages } from "../../utilities/errorMessages";
+import { privateRoutes } from "models/routes";
+import { successMessages } from "../../utilities/successMessages";
 
 export const useNews = () => {
   const [newsData, setNewsData] = useState([]);
   const [loadingNews, setLoadingNews] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const editHandler = (id) => {
+    navigate(`/${privateRoutes.BACKOFFICE}${privateRoutes.NEWSEDITFORM}${id}`);
+  };
+
+  const deleteHandler = async (id) => {
+    const find = newsData.find((element) => id === element.id);
+
+    if (find) {
+      const response = await AlertWarning("¿Estas segura/o que deseas eliminar la novedad?");
+
+      if (response) {
+        setLoading(true);
+        const activityDeleted = await privateService.deleted(URLs.news, id);
+
+        if (activityDeleted) {
+          await feedbackUser("center", "success", `${successMessages.deleteNews}`);
+          fetchNews();
+        } else {
+          await feedbackUser("center", "error", `${errorMessages.deleteNews}`);
+        }
+      }
+      setLoading(false);
+    }
+  };
+
+  const searchNewsHandler = async (searchText, selectedCategory) => {
+    const fetchParams = {};
+
+    if (selectedCategory.length > 0) {
+      fetchParams["category"] = selectedCategory;
+    }
+
+    if (searchText.length >= 3) {
+      fetchParams["search"] = searchText;
+    }
+
+    await fetchNews(fetchParams);
+  };
 
   const fetchNews = async (params = {}) => {
     try {
@@ -36,5 +81,5 @@ export const useNews = () => {
     fetchNews();
   }, []);
 
-  return { loadingNews, newsData, fetchNews };
+  return { loadingNews, editHandler, deleteHandler, searchNewsHandler, newsData, fetchNews };
 };
