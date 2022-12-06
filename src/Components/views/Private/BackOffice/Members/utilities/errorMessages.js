d8c335088c20a23d5c53d6bb9bac6ed464c60d9f
Yoshi Debat <yoshiwebdev@gmail.com>
2022-12-06 10:46:51 -0300

    agregar mensaje de error para el borrado de miembros

--- src/Components/views/Private/BackOffice/Members/utilities/errorMessages.js
+++ src/Components/views/Private/BackOffice/Members/utilities/errorMessages.js
@@ -3,4 +3,5 @@ export const errorMessages = {
   getMember: "Se produjo un error al obtener el miembro",
   createMember: "Se produjo un error al crear el miembro",
   editMember: "Se produjo un error al editar el miembro",
+  deleteMember: "Se produjo un error al borrar el miembro",
 };
