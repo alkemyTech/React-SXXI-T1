cc70dfb1aad49d8ce876953310a208a4c09a8e91
cris-al <albornozcristian94@gmail.com>
2022-12-05 23:45:14 -0300

    boton volver a backoffice y skeleton agregados a componente OrganizationData.jsx

--- src/Components/views/Private/Organization/OrganizationData/OrganizationData.jsx
+++ src/Components/views/Private/Organization/OrganizationData/OrganizationData.jsx
@@ -4,18 +4,22 @@ import {
   ContainerEditInf,
   Paragraph,
   ContainerImage,
-  Container1
+  Container1,
+  ButtonEdit,
 } from "./OrganizationDataStiled/OrganizationData.Styled";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import publicService from "Services/publicApiService";
 import { URLs } from "Services/ServicesURLS";
 import { feedbackUser } from "utilities/alerts/feedbackUser.util";
-import { CustomButton } from "Components/GlobalComponents/CustomButton/CustomButton";
 import { CustomTitle } from "Components/GlobalComponents/CustomTitle/CustomTitle";
+import { SkeletonLoader } from "Components/GlobalComponents/Loading/SkeletonLoader/SkeletonLoader";
+import { BackTo } from "Components/GlobalComponents/BackTo/BackTo";
+import { privateRoutes } from "models/routes";
 
 export default function OrganizationData() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [organizationData, setOrganizationData] = useState({
     name: "",
     image: "",
@@ -23,47 +27,62 @@ export default function OrganizationData() {
   });
 
   useEffect(() => {
-    const req = async () => {
-      const info = await publicService.get(URLs.organization);
-      if(info.success){
-        const {data} = info;
-        setOrganizationData({
-          name: data.name,
-          image: data.logo,
-          shortDescription: data.short_description
-        });
-      }else{
-        feedbackUser('center', 'error', 'Ha ocurrido un error');
-      }
-    }
-    req();
+    setLoading(true);
+    publicService
+      .get(URLs.organization)
+      .then((res) => {
+        if (res.success) {
+          const { data } = res;
+          setOrganizationData({
+            name: data.name,
+            image: data.logo,
+            shortDescription: data.short_description,
+          });
+        } else {
+          feedbackUser("center", "error", "Ha ocurrido un error");
+        }
+      })
+      .catch(() => feedbackUser("center", "error", "Ha ocurrido un error"))
+      .finally(() => setLoading(false));
   }, []);
 
   function toEdit() {
-    navigate('edit');
+    navigate("edit");
   }
 
   return (
     <Container>
-      <ContainerEditInf>
-        <h3>Datos de la Organización:</h3>
-        <CustomButton
-          buttonClass="col-12 col-sm-4 col-lg-3"
-          text='Editar Información'
-          background="success"
-          color="success"
-          onClick={toEdit}/>
-      </ContainerEditInf>
-      <Container1>
-      <CustomTitle wrapTextClass="text-center" title={organizationData.name} height='none'/>
-      
-      <ContainerImage>
-        <Image src={organizationData.image} alt={organizationData.name ? organizationData.name : 'Somos Más logo'} />
-      </ContainerImage>
-      <Paragraph
-        dangerouslySetInnerHTML={{ __html: organizationData.shortDescription }}
-      />
-      </Container1>
+      {loading ? (
+        <>
+          <SkeletonLoader xs={12} />
+          <SkeletonLoader xs={12} />
+          <SkeletonLoader xs={12} />
+          <SkeletonLoader xs={12} />
+          <SkeletonLoader xs={12} />
+          <SkeletonLoader xs={12} />
+          <SkeletonLoader xs={12} />
+        </>
+      ) : (
+        <>
+          <ContainerEditInf>
+            <BackTo text="Ir a Dashboard" wrapLink="col-12 col-sm-4 col-md-5 col-lg-3" to={"/" + privateRoutes.BACKOFFICE} />
+            <ButtonEdit className="col-12 col-sm-4 col-lg-3" onClick={toEdit}>
+              Editar Información
+            </ButtonEdit>
+          </ContainerEditInf>
+          <h3>Datos de la Organización:</h3>
+          <Container1>
+            <CustomTitle wrapTextClass="text-center" title={organizationData.name ? organizationData.name : "Sin titulo"} height="none" />
+
+            <ContainerImage>
+              <Image src={organizationData.image} alt={organizationData.name ? organizationData.name : "Sin logo"} />
+            </ContainerImage>
+            <Paragraph
+              dangerouslySetInnerHTML={{ __html: organizationData.shortDescription ? organizationData.shortDescription : "Sin descripción" }}
+            />
+          </Container1>
+        </>
+      )}
     </Container>
   );
 }
