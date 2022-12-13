7fc479303e522a6ccfade53df0d7a29518139df5
LuisGerezM <luis.gerez@gmail.com>
2022-12-13 19:24:11 -0300

    terminada page not found - ONG1-130

--- src/utilities/routesNoMatch.util.js
+++ src/utilities/routesNoMatch.util.js
@@ -1,11 +1,11 @@
+import PageNotFound from "Components/views/PageNotFound/PageNotFound";
 import { Route, Routes } from "react-router-dom";
 
 export const RoutesNoMatch = ({ children }) => {
   return (
     <Routes>
       {children}
-      {/* <Route path="*" element={<Navigate to={routes.HOME} />} /> */}
-      <Route path="*" element={<div>404 - not found</div>} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
