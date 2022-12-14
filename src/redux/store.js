5cec2e1159c28505d62bdffd601ef07b101c82aa
Luis Gerez Martinez <luis.gerez@gmail.com>
2022-12-14 15:03:13 -0300

    reducer de slides creado (#125)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import membersSlice from "./states/membersSlice";
 import userSliceReducer from "./states/user";
+import slidesSliceReducer from "./states/slides";
 import categoriesSlice from "./states/categoriesSlice";
 
 export default configureStore({
   reducer: {
     user: userSliceReducer,
+    slides: slidesSliceReducer,
     member: membersSlice,
     category: categoriesSlice,
   },
